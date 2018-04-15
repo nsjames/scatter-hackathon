e8a70fa0f861ef97d9ad0d4dd98318d7c96bb323
Shai James <skyandbeyond@gmail.com>
2018-04-15 23:07:38 +0300

    sponsors

--- src/blockProducers.js
+++ src/blockProducers.js
@@ -3,7 +3,7 @@ export default [
         name:'EOS',
         name2:' New York',
         img:'/assets/img/sponsors/eosnewyork.jpg',
-        url:'https://www.eosnewyork.io/',
+        url:'https://www.eosnewyork.io',
         bio:`
         EOS New York is a preeminent block producer candidate for the Eastern United States who believes the EOS.IO platform will usher in fundamental positive change for how we connect with one another.
         `,
@@ -12,7 +12,7 @@ export default [
         name:'EOS',
         name2:'42',
         img:'/assets/img/sponsors/eos42.jpg',
-        url:'https://www.eos42.io/',
+        url:'https://www.eos42.io',
         bio:`
         EOS42 will strive to empower the community, by investing in essential apps that EOS.io needs, helping build and support new dApps, and providing education to a wider audience on the benefits of EOS and decentralization.
         `,
@@ -21,7 +21,7 @@ export default [
         name:'EOS',
         name2:' Nation',
         img:'/assets/img/sponsors/eosnation.jpg',
-        url:'https://eosnation.io/',
+        url:'https://eosnation.io',
         bio:`
             EOS Nation is dedicated to fostering the EOS platform through reliable and efficient block production and community engagement. They believe in a future where life, liberty, and property is protected through decentralization.
         `,
@@ -39,7 +39,7 @@ export default [
         name:'EOS',
         name2:'phere',
         img:'/assets/img/sponsors/eosphere.jpg',
-        url:'http://eosphere.io/',
+        url:'http://eosphere.io',
         bio:`
             Professional integrity, transparency, commitment, cooperation and independence are all characteristics that the EOS community can expect from EOSphere.
         `,
@@ -48,7 +48,7 @@ export default [
         name:'EOS',
         name2:' Cannon',
         img:'/assets/img/sponsors/eoscannon.jpg',
-        url:'http://eoscannon.io/',
+        url:'http://eoscannon.io',
         bio:`
             EOS Cannon will be a stable block producer to serve the community. Futher, we plan to be an active EOS ecosystem contributor and project incubator. 
         `,
@@ -58,7 +58,7 @@ export default [
         name:'EOS',
         name2:' SoCal',
         img:'/assets/img/sponsors/eossocal.jpg',
-        url:'https://www.eosocal.io/',
+        url:'https://www.eosocal.io',
         bio:`
             EOSoCal is a group founded in Nov 2017 with a mission of supporting and promoting the successful launch and viability of the EOS.IO software and community, operating out of Southern California. 
         `,
@@ -68,7 +68,7 @@ export default [
         name:'Blockgenic',
         name2:' ',
         img:'/assets/img/sponsors/blockgenic.jpg',
-        url:'http://www.blockgenic.io/',
+        url:'http://www.blockgenic.io',
         bio:`
             We are a block producer candidate based out of the Pacific North West, headquartered in Seattle. Our group of technology entrepreneurs and investors believe in the power of EOS and blockchain technology to make a positive long term impact around the world.  
         `,
@@ -78,7 +78,7 @@ export default [
         name:'EOS',
         name2:' Canada',
         img:'/assets/img/sponsors/eoscanada.jpg',
-        url:'http://www.blockgenic.io/',
+        url:'http://www.blockgenic.io',
         bio:`
             We are a block producer candidate based out of the Pacific North West, headquartered in Seattle. Our group of technology entrepreneurs and investors believe in the power of EOS and blockchain technology to make a positive long term impact around the world.  
         `,
@@ -88,7 +88,7 @@ export default [
         name:'EOS',
         name2:'YS',
         img:'/assets/img/sponsors/eosys.jpg',
-        url:'http://eosys.io/',
+        url:'http://eosys.io',
         bio:`
             EOSYS is the First EOSIO Block Producer Candidate from Korea. EOSYS also created EOSscan.io in June of 2017, one of the most pivotal applications surrounding EOSIO.  
         `,
@@ -98,7 +98,7 @@ export default [
         name:'EOS',
         name2:'Cafe',
         img:'/assets/img/sponsors/eoscafe.jpg',
-        url:'http://eos.cafe/',
+        url:'http://eos.cafe',
         bio:`
             EOS Cafe is a community of EOS enthusiasts. Our continuing mission is to build collaboration spaces online, on blockchain, and in the real world.
         `,
@@ -108,7 +108,7 @@ export default [
         name:'eos',
         name2:'Dac',
         img:'/assets/img/sponsors/eosdac.jpg',
-        url:'https://eosdac.io/',
+        url:'https://eosdac.io',
         bio:`
             eosDac is a decentralized and community-owned EOSIO Block Producer. The DAC is controlled by its token holders and the board members they vote to run its operations.
         `,
@@ -118,7 +118,7 @@ export default [
         name:'MEET',
         name2:'.ONE',
         img:'/assets/img/sponsors/meetone.jpg',
-        url:'https://meet.one/',
+        url:'https://meet.one',
         bio:`
             MEET.ONE is a portal to EOS ecology. MEET is a kind of EOS ecological gateway allowing you to take part in EOS ecological development.
         `,
--- src/dappsAndCommunity.js
+++ src/dappsAndCommunity.js
@@ -9,25 +9,25 @@ export default [
         name:'EOS',
         name2:' Go',
         img:'/assets/img/sponsors/eosgo.jpg',
-        url:'https://forums.eosgo.io/',
+        url:'https://forums.eosgo.io',
         bio:'EOS Go is the first source for EOS and a neutral community organization, dedicated to supporting the growth and development of all EOS public blockchains.'
     },{
         name:'Koreos',
         name2:'',
         img:'/assets/img/sponsors/koreos.jpg',
-        url:'http://koreos.io/',
+        url:'http://koreos.io',
         bio:'KOREOS.IO has built the most powerful EOS community in South Korea. They have a huge community and spread the latest EOS news in Korean via telegram, tweets, and their website.'
     },{
         name:'Chintai',
         name2:'',
         img:'/assets/img/sponsors/chintai.jpg',
-        url:'https://chintai-eos.io/',
+        url:'https://chintai-eos.io',
         bio:'Chintai is a fully functional token leasing platform that will allow EOS token holders to generate income through token leasing, while in turn providing critical resources needed by dApp developers.'
     },{
         name:'EOS',
         name2:' Index',
         img:'/assets/img/sponsors/eosindex.jpg',
-        url:'https://eosindex.io/',
+        url:'https://eosindex.io',
         bio:'EOS Index is a directory of projects/dapps based around EOS and a job platform helping projects to find developers and developers to find work.'
     },{
         name:'Jeff',
@@ -39,7 +39,7 @@ export default [
         name:'Carmel',
         name2:'',
         img:'/assets/img/sponsors/carmel.jpg',
-        url:'https://www.carmel.io/',
+        url:'https://www.carmel.io',
         bio:`
             The Decentralized Open Source Tech Education Platform that helps aspiring and seasoned Software Developers grow their skill set and track their growth through tokenized peer challenges.
         `
